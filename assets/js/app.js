c51581c10ee87fc27598b3703fc292afce10d3b4
Reuben Ingber <reuben.ingber@expandtheroom.com>
2015-02-13 15:58:03 -0500

    Tweaks & images

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,30 +7,31 @@
             title: "Hospital Worker Who Handled Ebola Samples Is on Cruise Ship",
             rank: "1",
             url: "#",
-            img: "http://placehold.it/165x125"
+            img: "assets/img/img_1.jpg"
         },
         {
             title: "Why Dr. Kent Brantly Couldn't Donate Blood to Thomas Eric...",
             rank: "2",
             url: "#",
-            img: "http://placehold.it/165x125"
+            img: "assets/img/img_2.jpg"
         },
         {
             title: "Joan Rivers' Cause of Death Revealed",
             rank: "3",
             url: "#",
-            img: "http://placehold.it/165x125"
+            img: "assets/img/img_3.jpg"
         },
         {
             title: "Ebola Nurse Goes From Good to Fair After Trip to NIH",
             rank: "4",
             url: "#",
-            img: "http://placehold.it/165x125"
+            img: "assets/img/img_4.jpg"
         },
         {
             title: "Nurse With Ebola Appears on Camera for 1st Time",
             rank: "5",
-            url: "#"
+            url: "#",
+            img: "assets/img/img_5.jpg"
         },
     ];
 
